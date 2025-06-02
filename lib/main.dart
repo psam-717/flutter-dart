2e54edd4f9fe4e28b302a5de2b51703de9b9d136
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-06-02 21:30:04 +0000

    called functiontions to main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,26 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'widgets/horizontal_names_list.dart';
+import 'widgets/vertical_names_list.dart';
 
 void main() { // entry point of the file
-  runApp(const MyApp());
+  runApp(MyApp());
   // runApp starts the app
   // MyApp() is a widget
 }
 
-class MyApp extends StatelessWidget{
-  const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  
 
   @override
   Widget build(BuildContext context) {
-
+    // TODO: implement build
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.grey[400],
-        body: ListView.builder( 
-          itemCount: 20,
-          itemBuilder: (context,index) => ListTile(
-            textColor: Colors.black,
-            title: Text(index.toString()),
-          ))
+       body: HorizontalNamesList(),
       ),
     );
   }
