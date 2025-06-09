fea89d2387fbfd40ec52e6e6a5758dbc690a3f37
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-06-09 06:13:30 +0000

    normal page with stateless widget

---
+++ lib/pages/page_two.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class SecondPage extends StatelessWidget{
+  SecondPage({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    
+    return Scaffold(
+      appBar: AppBar(title: Text("Second page"), backgroundColor: Colors.amber,),
+      body: Center(),
+    );
+  }
+}
