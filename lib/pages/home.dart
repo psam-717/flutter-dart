f794cfefa17daee27be674945bd0a83d0c4ed1f2
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-06-09 06:11:10 +0000

    home screen

---
+++ lib/pages/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  HomePage({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child: Text("Home page"),
+      )
+    );
+  }
+}
