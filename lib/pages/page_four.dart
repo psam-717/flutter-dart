ef181db811d3184ef72079fadc16095d66742e0e
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-06-09 06:11:44 +0000

    fourth page

---
+++ lib/pages/page_four.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FourthPage extends StatelessWidget{
+  FourthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text('Fourth Page'), backgroundColor: Colors.blueGrey[400],),
+      body: Center(),
+    );
+  }
+}
